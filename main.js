7b0f8b0a089ec6e04d6a2e81b411572888eb24bf
aneena ansar <131254828+aneenaansar@users.noreply.github.com>
2025-02-04 12:03:48 +0530

    updated

--- main.js
+++ main.js
@@ -1,52 +1,3 @@
-// var tl = gsap.timeline({scrollTrigger:{
-//     trigger:".two",
-//     start:"0% 95%",
-//     end:"80% 60%",
-//     scrub:true
-// }})
-
-// tl.to("#heroimage",{
-//     top:"120%",
-//     left:"77%",
-//     width:"30%",
-//     rotate:"-30deg"
-// },'heroimage')
-
-// // tl.to("#oven",{
-// //     top:"153%",
-// //     rotate:"0deg",
-// //     left:"75%",
-// //     width:"12%"
-// // },'heroimage')
-
-// // tl.to("#beater",{
-// //     top:"114%",
-// //     rotate:"100deg",
-// //     left:"5%",
-// //     width:"5%"
-// // },'heroimage')
-
-// // tl.to("#tray",{
-// //     top:"125%",
-// //     rotate:"100deg",
-// //     left:"83%",
-// //     width:"12%"
-// // },'heroimage')
-
-// var tl2 = gsap.timeline({scrollTrigger:{
-//     trigger:".three",
-//     start:"0% 95%",
-//     end:"20% 50%",
-//     scrub:true
-// }})
-
-// tl2.to("#heroimage",{
-//     top:"212%",
-//     left:"50%",
-//     width:"30%",
-//     rotate:"0deg"
-// },'heroimage')
-
 var tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".two",
@@ -82,3 +33,11 @@ tl2.to("#heroimage", {
     rotate: "0deg",
     ease: "power1.out" // Added ease function
 }, 'heroimage');
+
+
+
+
+// Mobile menu toggle
+document.querySelector('.mobile-menu').addEventListener('click', () => {
+    document.querySelector('.nav_links').classList.toggle('active');
+});
